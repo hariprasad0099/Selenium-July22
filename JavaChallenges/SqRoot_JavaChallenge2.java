7f1674493ed9736154c3cabeb1165ad18696cd8b
hariprasad0099 <85383391+hariprasad0099@users.noreply.github.com>
2022-08-02 22:08:24 +0530

    Add files via upload

---
+++ JavaChallenges/SqRoot_JavaChallenge2.java
@@ -0,0 +1,42 @@
+package javaPractice;
+
+public class SqRoot_JavaChallenge2 {
+
+	public static void main(String[] args) {
+		
+		//declare the number to find sqrt
+		int num = 0;
+		
+		int x ;
+		
+		for (int i =0 ; i <= num; i++) {
+			
+			x = i*i;
+			
+			if(x==i) {
+				System.out.println( " The number '"+num +"' itself the sqrt");
+				break;
+			}
+			
+			if (x < num) {
+				i++;			
+			}
+			
+			if (x == num) {
+				System.out.println("The sq root here is " + i);;
+				break;			
+			}
+			
+			if (x > num) {
+				System.out.println("The sq root is  " + (i-1));
+				break;			
+			}
+			
+		}
+
+		
+		
+
+	}
+
+}
